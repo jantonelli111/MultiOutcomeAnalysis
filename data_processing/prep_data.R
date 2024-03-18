172aa6c6359f852f74cae8ae1031a944b8d228d8
audiracmichelle@gmail.com <audiracmichelle@example.com>
2024-03-18 07:26:09 -0400

    modified file extension

--- data_processing/prep_data.R
+++ data_processing/prep_data.R
@@ -16,6 +16,7 @@ multioutcome_df <- read_parquet("data/input/MultiOutcomeAnalysis/multioutcome.pa
 df <- covars_df %>% 
   left_join(multioutcome_df)
 
+dim(df)
 colnames(df)
 # [1] "zip"                    "year"                   "pm25_ensemble"          "mean_bmi"              
 # [5] "smoke_rate"             "hispanic"               "pct_blk"                "medhouseholdincome"    
@@ -37,4 +38,27 @@ colnames(df)
 # [69] "stroke_rate"
 
 # write processed data
-write_csv(df, "data/processed/outcomes_merged_all_years.csv")
+write_parquet(df, "data/processed/outcomes_merged_all_years.parquet")
+
+# compare columns with fake data
+fake_df <- read_csv("data/fake_outcomes_merged_all_years.csv")
+dim(fake_df)
+colnames(fake_df)
+# [1] "zip"                                        "year"                                       "pm25_ensemble"                              "mean_bmi"                                  
+# [5] "smoke_rate"                                 "hispanic"                                   "pct_blk"                                    "medhouseholdincome"                        
+# [9] "medianhousevalue"                           "poverty"                                    "education"                                  "pct_owner_occ"                             
+# [13] "summer_tmmx"                                "winter_tmmx"                                "summer_rmax"                                "winter_rmax"                               
+# [17] "pm25"                                       "ozone"                                      "br"                                         "ca"                                        
+# [21] "cu"                                         "ec"                                         "fe"                                         "k"                                         
+# [25] "nh4"                                        "ni"                                         "no3"                                        "oc"                                        
+# [29] "pb"                                         "si"                                         "so4"                                        "v"                                         
+# [33] "z"                                          "death_rate"                                 "female_percentage"                          "dual_percentage"                           
+# [37] "mean_age"                                   "percentage_race_labelAsian"                 "percentage_race_labelBlack"                 "percentage_race_labelOther"                
+# [41] "percentage_race_labelWhite"                 "percentage_race_labelHispanic"              "percentage_race_labelNorth_American_Native" "anemia_rate"                               
+# [45] "ami_rate"                                   "alzh_rate"                                  "alzhdmta_rate"                              "atrialfb_rate"                             
+# [49] "cataract_rate"                              "chrnkidn_rate"                              "copd_rate"                                  "chf_rate"                                  
+# [53] "diabetes_rate"                              "stroke_rate"                                "breastCancer_rate"                          "colorectalCancer_rate"                     
+# [57] "prostateCancer_rate"                        "lungCancer_rate"                            "endometrialCancer_rate"                     "hyperp_rate"                               
+# [61] "glaucoma_rate"                              "hipfrac_rate"                               "ischmcht_rate"                              "depressn_rate"                             
+# [65] "osteoprs_rate"                              "ra_oa_rate"                                 "asthma_rate"                                "hyperl_rate"                               
+# [69] "hypert_rate"                                "hypoth_rate"  
